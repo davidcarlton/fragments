899fe81aeb67afd13fdea1d0d3d5793d97bf138d
David Carlton <carlton@bactrian.org>
2011-11-12 21:47:44 -0800

    A single publish.sh script.

--- bin/publish.sh
+++ bin/publish.sh
@@ -2,25 +2,21 @@
 
 set -e
 
-if [ "$#" != "2" ]; then
-  echo "Usage: publish.sh LOCATION ITEM_NAME"
+if [ "$#" != "1" ]; then
+  echo "Usage: publish.sh ITEM_NAME"
   exit 1
 fi
 
-LOCATION=$1
-
-# People can pass in either item_name or location/item_name.
-ITEM_NAME=$(basename $2)
+# fragments/name-of-fragment or mosaics/name-of-mosaic
+ITEM_NAME=$1
 
 cd $(dirname $0)/..
 # FIXME (2011-11-05, carlton): Make this parameterizable.
 SOURCE_DIR=${PWD}
-mkdir -p ${SOURCE_DIR}/published/${LOCATION}
 
-if [ ! -e "${SOURCE_DIR}/${LOCATION}/${ITEM_NAME}" ]; then
+if [ ! -e "${SOURCE_DIR}/${ITEM_NAME}" ]; then
   echo "Item ${ITEM_NAME} does not exist."
   exit 1
 fi
 
-# This is the format that the atom standard recommends.
-date -u "+%Y-%m-%dT%H:%M:%SZ" > ${SOURCE_DIR}/published/${LOCATION}/${ITEM_NAME}
+date -u "+%Y-%m-%dT%H:%M:%SZ" > ${SOURCE_DIR}/published/${ITEM_NAME}
