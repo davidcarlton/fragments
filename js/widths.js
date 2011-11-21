b5308505dc3dd2468e146b2c9d25f09f072a5070
David Carlton <carlton@bactrian.org>
2011-11-20 20:25:37 -0800

    Reduced lower bound for medium width.

--- js/widths.js
+++ js/widths.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   var width = $(window).width();
   if (width < 500) {
     $("body").addClass("small");
-  } else if (width < 1050) {
+  } else if (width < 900) {
     $("body").addClass("medium");
   }
 })
